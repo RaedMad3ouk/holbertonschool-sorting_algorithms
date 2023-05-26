312c298e6421c1e4dc1e01acf72998ed4ee84ee4
raed <raed.da3k@gmail.com>
2023-05-27 00:17:49 +0100

    add func header

--- sort.h
+++ sort.h
@@ -20,7 +20,8 @@ typedef struct listint_s
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
 
 
 #endif
